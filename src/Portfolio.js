f705ad3ee15416c9b62f87f48db0d5306cec5847
Martin <martinmarescr@gmail.com>
2023-01-15 01:24:15 -0600

    Form.js created
    Form created and data provided in order to function. Still to style

---
+++ src/Portfolio.js
@@ -0,0 +1,13 @@
+import './Portfolio.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Portfolio() {
+    return (
+        <section className='portfolio'>
+            <h2>Portfolio</h2>
+            
+        </section>
+    );
+}
+
+export default Portfolio;
